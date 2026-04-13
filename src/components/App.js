1bb17eaf984caf11d34d7fcd216b065e9f7fc780
Abhilash Jha <abhilashjha121@gmail.com>
2026-04-13 23:38:12 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Child from "./Child";
 
 const App = () => {
+  const data=[{todo:"Learn React",status:true},{todo:"Build a React app",status:true} , {todo:"Deploy the React app",status:true}];
+  const [todos,setTodos]=useState(data);
+console.log(todos);
+  function handleComplete(id){
+    setTodos(todos.map((todo,idx)=>(idx ===id ? {...todo,status:false}:todo)));
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <Child todos={todos} handleComplete={handleComplete}/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Child.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Child({todos,handleComplete}) {
+    // function handleComplete(id){
+    //     todos.filter((todo,idx)=>idx ==id);
+    // }
+  return (
+    <div>
+        <h2>Child Component</h2>
+        <ul>
+            {todos.map((todo, idx)=>(
+                <li key={idx}>{todo.todo}
+                {todo.status ? <button onClick={()=>handleComplete(idx)}>Complete</button>:null}
+                
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
