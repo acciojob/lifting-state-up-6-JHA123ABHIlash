dc299b7499569c373e1317404d5d74b2a8ace17b
Abhilash Jha <abhilashjha121@gmail.com>
2026-04-14 00:14:15 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,8 @@ const App = () => {
   const [todos,setTodos]=useState([{id:1,todo:"Learn React",status:true },{id:2,todo:"Build a React app",status:true} , {id:3,todo:"Deploy the React app",status:true}]);
 
   function handleComplete(id){
-    let updatedTodo=todos.map((todo)=>(todo.id ===id ? {...todo,status:false }:todo));
-    setTodos(updatedTodo);
+    
+    setTodos(prev=>prev.map((todo)=>(todo.id ===id ? {...todo,status:false }:todo)));
   }
   return (
     <div>
